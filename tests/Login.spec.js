2b2da090d2d5d1c6b7d3668185d193abb6476a0f
bikashmaz <148486924+bikashmaz@users.noreply.github.com>
2026-01-10 21:19:11 +0530

    Initial project upload

---
+++ tests/Login.spec.js
@@ -0,0 +1,51 @@
+import { test, expect } from '@playwright/test';
+
+test('No Credentials', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await expect(page.getByRole('img', { name: 'company-branding' })).toBeVisible();
+  await page.getByRole('button', { name: 'Login' }).click();
+  const requiredErrors = page.getByText('Required');
+  await expect(requiredErrors.nth(0)).toBeVisible();
+  await expect(requiredErrors.nth(1)).toBeVisible();
+});
+
+test('Only Username', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await expect(page.getByRole('img', { name: 'company-branding' })).toBeVisible();
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByText('Required')).toBeVisible();
+});
+
+test('Only Password', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await expect(page.getByRole('img', { name: 'company-branding' })).toBeVisible();
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByText('Required')).toBeVisible();
+});
+
+test('Wrong Credentials', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await expect(page.getByRole('img', { name: 'company-branding' })).toBeVisible();
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('admin');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByText('Invalid credentials')).toBeVisible();
+});
+
+test('Valid Credentials', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await expect(page.getByRole('img', { name: 'company-branding' })).toBeVisible();
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
+});
+
